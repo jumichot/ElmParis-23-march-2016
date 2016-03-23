7b1adb423e682817cd8352c671b687f4ff18d6bb
julien michot <ju.michot@gmail.com>
2016-03-23 16:14:43 +0100

    Add first html element to display the application

--- Counter.elm
+++ Counter.elm
@@ -1,3 +1,12 @@
 import Html exposing(..)
+import Html.Attributes exposing(..)
+
+-- Spec du projet : faire un compteur
+-- [Decrement] 0 [Increment]
+
 main =
-  text "Hello World !"
+  div [ class "container" ] [
+    button [] [ text "Decrement" ]
+    , text "0"
+    , button [] [ text "Increment" ]
+  ]
