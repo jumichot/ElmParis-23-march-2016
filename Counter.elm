820a64a95e5132de4651cdb55ff84afa145f55d8
julien michot <ju.michot@gmail.com>
2016-03-23 16:27:07 +0100

    Add fake elm architecture

--- Counter.elm
+++ Counter.elm
@@ -4,9 +4,26 @@ import Html.Attributes exposing(..)
 -- Spec du projet : faire un compteur
 -- [Decrement] 0 [Increment]
 
-main =
+-- ELM ARCHITECTURE
+-- MODEL : Modélise l'état de notra application
+type alias Model = Int
+
+initialModel : Model
+initialModel = 10
+
+-- VIEW : prend un model, et assure le rendu 
+view  model =
   div [ class "container" ] [
     button [] [ text "Decrement" ]
-    , text "0"
+    , text (toString model)
     , button [] [ text "Increment" ]
   ]
+
+-- UPDATE : prend un model, le met à jour, et retourne un nouveau modèle
+update model =
+  model + 20
+
+
+main : Html
+main =
+  view (update initialModel)
